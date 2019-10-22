5d0d8e2ab13710258fb51ee152c7e462ad1f8122
Multi-ITKMITL <48679839+Multi-ITKMITL@users.noreply.github.com>
2019-10-22 11:26:08 +0700

    update 22/10/2562
    update 22/10/2562

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+require 'vendor/autoload.php';
+?>
+<html lang="en">
+
+<head>
+    <title>MYSTERYEARTH Test PHP</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/coliff/bootstrap-rfs/bootstrap-rfs.css">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+  <script src="script.js"></script>
+</head>
+<body>
+
+</body>
